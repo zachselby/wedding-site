635fdb5fe2bf20ec5927a670f22449e8385d9f72
Zach Selby <zach.selby@razorfish.com>
2017-01-26 02:02:42 -0800

    Committing all changes for initial release. Sending invitations tomorrow :D

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,16 @@
-console.log('\'Allo1 \'Allo!');
+console.log('\'Allo1s \'Allo!');
 
-$(".arc").arctext({radius:600});
+$(function(){
+    var now = Math.floor(Date.now() / 1000);
+    var then = 1488499200;
+    $('#countdown .days').html(Math.round((then - now) / (60 * 60 * 24)));
+});
 
-$(".lazy").Lazy({
+$('.arc').arctext({radius:600});
+
+$('.lazy').Lazy({
     visibleOnly:true,
     effect:'fadeIn',
     effectTime:600
 });
+
