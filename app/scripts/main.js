017f59f4fe3411275f19685a95ff0fa7f9fdb248
Zach Selby <zach.selby@razorfish.com>
2017-01-28 18:37:59 -0800

    Added hero image of the happy couple to header. Some spacing fixes. Added dividers between sections to make it more legible.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,13 @@
-console.log('\'Allo1s \'Allo!');
-
 $(function(){
     var now = Math.floor(Date.now() / 1000);
     var then = 1488499200;
     $('#countdown .days').html(Math.round((then - now) / (60 * 60 * 24)));
 });
 
-$('.arc').arctext({radius:600});
+//$('.arc').arctext({radius:600});
 
 $('.lazy').Lazy({
-    visibleOnly:true,
+    delay: false,
     effect:'fadeIn',
     effectTime:600
 });
